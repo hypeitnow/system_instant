ece509172885c9d85087f1b154fd30a043140db2
stojshic <stojshic@arch>
2022-01-24 18:22:21 +0100

    Added option to choose AUR helper

---
+++ 2-user.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+echo -ne "
+-------------------------------------------------------------------------
+   █████╗ ██████╗  ██████╗██╗  ██╗████████╗██╗████████╗██╗   ██╗███████╗
+  ██╔══██╗██╔══██╗██╔════╝██║  ██║╚══██╔══╝██║╚══██╔══╝██║   ██║██╔════╝
+  ███████║██████╔╝██║     ███████║   ██║   ██║   ██║   ██║   ██║███████╗
+  ██╔══██║██╔══██╗██║     ██╔══██║   ██║   ██║   ██║   ██║   ██║╚════██║
+  ██║  ██║██║  ██║╚██████╗██║  ██║   ██║   ██║   ██║   ╚██████╔╝███████║
+  ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝   ╚═╝   ╚═╝   ╚═╝    ╚═════╝ ╚══════╝
+-------------------------------------------------------------------------
+                    Automated Arch Linux Installer
+                        SCRIPTHOME: ArchTitus
+-------------------------------------------------------------------------
+
+Installing AUR Softwares
+"
+# You can solve users running this script as root with this and then doing the same for the next for statement. However I will leave this up to you.
+source $HOME/ArchTitus/setup.conf
+
+  cd ~
+  touch "~/.cache/zshhistory"
+  git clone "https://github.com/ChrisTitusTech/zsh"
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+  ln -s "~/zsh/.zshrc" ~/.zshrc
+
+if [[ ! $AUR_HELPER == none ]]; then
+  cd ~
+  git clone "https://aur.archlinux.org/$AUR_HELPER.git"
+  cd ~/$AUR_HELPER
+  makepkg -si --noconfirm
+  $AUR_HELPER -S --noconfirm --needed - < ~/ArchTitus/pkg-files/aur-pkgs.txt
+fi
+
+export PATH=$PATH:~/.local/bin
+cp -r ~/ArchTitus/dotfiles/* ~/.config/
+pip install konsave
+konsave -i ~/ArchTitus/kde.knsv
+sleep 1
+konsave -a kde
+
+echo -ne "
+-------------------------------------------------------------------------
+                    SYSTEM READY FOR 3-post-setup.sh
+-------------------------------------------------------------------------
+"
+exit
