be6e497225f5fd773db7a2ccfbf8fa3159c006ac
hypeit <matkoz111@gmail.com>
2022-05-15 06:46:08 +0200

    perfect brave extract

--- ArchTitus/configs/brave_extract.sh
+++ ArchTitus/configs/brave_extract.sh
@@ -1,6 +1,6 @@
 #!/bin/bash 
 
-tar --overwrite --strip-components=1 -C /home/hypeit/.cache/BraveSoftware -xzf /tmp/brave.tgz tmp/BraveSoftware 
+tar --overwrite --strip-components=1 -C /home/hypeit/.cache -xzf /tmp/brave.tgz tmp/BraveSoftware 
 
-tar --overwrite --strip-components=1 -C /home/hypeit/.config/BraveSoftware/Brave-Browser -xzf /tmp/brave.tgz tmp/Brave-Browser 
+tar --overwrite --strip-components=1 -C /home/hypeit/.config/BraveSoftware -xzf /tmp/brave.tgz tmp/Brave-Browser 
 
