6d96a8e914359ad8aa1816bbb01de40282ccc0a7
hypeit <matkoz111@gmail.com>
2022-05-21 13:25:21 +0200

    refinement

--- ArchTitus/configs/brave_extract.sh
+++ ArchTitus/configs/brave_extract.sh
@@ -1,6 +1,13 @@
 #!/bin/bash 
 
-tar --overwrite --strip-components=1 -C /home/hypeit/.cache -xzf /tmp/brave.tgz tmp/BraveSoftware 
+echo "Starting the extraction proccess..."
 
-tar --overwrite --strip-components=1 -C /home/hypeit/.config/BraveSoftware -xzf /tmp/brave.tgz tmp/Brave-Browser 
+[[ -f /tmp/brave.tgz ]] || echo "Exiting as no archive was found"; exit 1 
 
+tar --overwrite --strip-components=1 -C "/home/hypeit/.cache/" -xzf /tmp/brave.tgz tmp/BraveSoftware || echo "Error in unpacking .cache"
+
+tar --overwrite --strip-components=1 -C "/home/hypeit/.config/BraveSoftware/" -xzf /tmp/brave.tgz tmp/Brave-Browser || echo "Error in unpacking .config"
+
+echo "Ended the extraction proccess..."
+
+ls -lath /home/hypeit/.cache/BraveSoftware /home/hypeit/.config/BraveSoftware/Brave-Browser 
