e301999941e6406023c809cd25bc57173d4013c9
HypeIt <matkoz111@gmail.com>
2023-04-02 08:27:14 +0200

    fixed locale gen for PL

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -48,10 +48,11 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+sed -i 's/^#pl_PL.UTF-8 UTF-8/pl_PL.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 timedatectl --no-ask-password set-timezone ${TIMEZONE}
 timedatectl --no-ask-password set-ntp 1
-localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
+localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_MEASUREMENT="pl_PL.UTF-8" LC_TIME="pl_PL.UTF-8" LC_NAME="pl_PL.UTF-8" LC_TELEPHONE="pl_PL.UTF-8"
 ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 # Set keymaps
 localectl --no-ask-password set-keymap ${KEYMAP}
