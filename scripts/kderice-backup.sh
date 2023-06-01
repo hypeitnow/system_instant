7e65e0102ef6c1b3bdeca26044631eb88f0f8570
Hype It <matkoz111@gmail.com>
2023-06-01 23:01:29 +0200

    konsave profile

--- scripts/kderice-backup.sh
+++ scripts/kderice-backup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cp -r $HOME/.config/kitty $HOME/$SCRIPTHOME/configs/.config/kitty
+cp "$PWD/../configs/conf.yaml" "$HOME/.config/konsave/conf.yaml"
 konsave -s current
 konsave -e current
