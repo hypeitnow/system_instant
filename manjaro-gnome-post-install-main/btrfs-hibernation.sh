b93e12ee6c3962d8c3f98c24bd636b211aeea72b
hypeit <matkoz111@hotmail.com>
2024-03-18 22:34:39 +0100

    working in garuda

--- manjaro-gnome-post-install-main/btrfs-hibernation.sh
+++ manjaro-gnome-post-install-main/btrfs-hibernation.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
+set -e 
 exec >2&
 
+pacman -Sy bc
 # Get the UUID of the system drive
-fs_uuid=$(findmnt / -o UUID -n) && echo ${fs_uuid}
+fs_uuid=$(findmnt / -o UUID -n) && echo "${fs_uuid}"
 
 # temporarily mount the system drive root via the on-demand line in fstab. if doesn't exist, create a temp mountpoint and mount
 if sudo grep -Fq "/mnt/drives/system" /etc/fstab; then sudo mount /mnt/drives/system; 
 else 
 sudo mkdir -p /mnt/drives/system
-sudo mount /mnt/drives/system
+sudo mount -U "${fs_uuid}" /mnt/drives/system
 fi
 
 # Create swap subvolume, then remove temp mountpoint
@@ -16,14 +18,14 @@ sudo btrfs subvolume create /mnt/drives/system/@swap
 sudo umount /mnt/drives/system
 
 # Temporarily mount the subvolume
-sudo mkdir /swap
-sudo mount -m -U ${fs_uuid} -o subvol=@swap,nodatacow /swap
+[[ -d /swap ]] || sudo mkdir /swap
+sudo mount -m -U "${fs_uuid}" -o subvol=@swap,nodatacow /swap
 
 # Create the swapfile
 sudo touch /swap/swapfile
 sudo chattr +C /swap/swapfile
-swp_size=$(echo "$(grep "MemTotal" /proc/meminfo | tr -d "[:blank:],[:alpha:],:") * 1.6 / 1000" | bc ) && echo $swp_size
-sudo dd if=/dev/zero of=/swap/swapfile bs=1M count=$swp_size status=progress
+swp_size=$(echo "$(grep "MemTotal" /proc/meminfo | tr -d "[:blank:],[:alpha:],:") * 1.6 / 1000" | bc ) && echo "${swp_size}"
+sudo dd if=/dev/zero of=/swap/swapfile bs=1M count="${swp_size}" status=progress
 sudo chmod 0600 /swap/swapfile
 sudo mkswap /swap/swapfile
 sudo umount /swap
@@ -38,21 +40,20 @@ swapon -s
 
 # Configure hibernation
 # Get swap device uuid and offset
-swp_uuid=$(findmnt -no UUID -T /swap/swapfile) && echo $swp_uuid
+swp_uuid=$(findmnt -no UUID -T /swap/swapfile) && echo "${swp_uuid}"
 cd /tmp
 curl -s "https://raw.githubusercontent.com/osandov/osandov-linux/master/scripts/btrfs_map_physical.c" > bmp.c
 gcc -O2 -o bmp bmp.c
-swp_offset=$(echo "$(sudo ./bmp /swap/swapfile | egrep "^0\s+" | cut -f9) / $(getconf PAGESIZE)" | bc) && echo $swp_offset
+swp_offset=$(echo "$(sudo ./bmp /swap/swapfile | egrep "^0\s+" | cut -f9) / $(getconf PAGESIZE)" | bc) && echo "${swp_offset}"
 # Add these values to grub
 echo -e "GRUB_CMDLINE_LINUX_DEFAULT+=\" resume=UUID=$swp_uuid resume_offset=$swp_offset \"" | sudo tee -a /etc/default/grub
-# add resume to mkinitcpio
-echo -e "HOOKS+=\" resume \"" | sudo tee -a /etc/mkinitcpio.conf
+
 # configure hibernation related systemd services
 sudo mkdir -pv /etc/systemd/system/{systemd-logind.service.d,systemd-hibernate.service.d}
 echo -e "[Service]\nEnvironment=SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK=1" | sudo tee /etc/systemd/system/systemd-logind.service.d/override.conf
 echo -e "[Service]\nEnvironment=SYSTEMD_BYPASS_HIBERNATION_MEMORY_CHECK=1" | sudo tee /etc/systemd/system/systemd-hibernate.service.d/override.conf
 # update initcpio and grub
-sudo mkinitcpio -P && sudo grub-mkconfig -o /boot/grub/grub.cfg 
+sudo grub-mkconfig -o /boot/grub/grub.cfg 
 
 # Use the modern way of standby: suspend-then-hibernate
 # Contrary to what is written here: 
@@ -60,20 +61,12 @@ sudo mkinitcpio -P && sudo grub-mkconfig -o /boot/grub/grub.cfg
 # follow https://man.archlinux.org/man/sleep.conf.d.5
 # These instructions do not work to enable suspend then hibernate, because "suspend-then-hibernate" is not actually a working value in these conf files.
 # This workaround does: 
-ln -s /usr/lib/systemd/system/systemd-suspend-then-hibernate.service /etc/systemd/system/systemd-suspend.service
+sudo ln -s /usr/lib/systemd/system/systemd-suspend-then-hibernate.service /etc/systemd/system/systemd-suspend.service
 # when suspend-then-hibernate is used, go into hibernation (0.0 power consumption) after 120min of suspend unless interrupted
-sed -i -e 's@#HibernateDelaySec=180min@HibernateDelaySec=120min@g' /etc/systemd/sleep.conf
+sudo sed -i -e 's@#HibernateDelaySec=180min@HibernateDelaySec=120min@g' /etc/systemd/sleep.conf
 
 # Now define what to do on user initiated actions: go into hibernation when hitting power key
-sed -i -e 's@#HandlePowerKey=poweroff@HandlePowerKey=hibernate@g' /etc/systemd/logind.conf
+sudo sed -i -e 's@#HandlePowerKey=poweroff@HandlePowerKey=hibernate@g' /etc/systemd/logind.conf
 # Use suspend-then-hibernate when lid is closed, even when on external power since you could disconnect from power during suspend
-sed -i -e 's@HandleLidSwitch=ignore@HandleLidSwitch=suspend@g' /etc/systemd/logind.conf
-sed -i -e 's@#HandleLidSwitchExternalPower=suspend@HandleLidSwitchExternalPower=suspend@g' /etc/systemd/logind.conf
-
-# Gnome specific: 
-# Power
-gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'hibernate'
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 3600
-gsettings set org.gnome.shell.extensions.arcmenu power-options "[(0, true), (1, true), (2, true), (3, true), (4, true), (5, false), (6, true)]"
-
-echo "Please reboot before using standby or hibernate. Hit a key to continue the post-install script."
+sudo sed -i -e 's@HandleLidSwitch=ignore@HandleLidSwitch=suspend@g' /etc/systemd/logind.conf
+sudo sed -i -e 's@#HandleLidSwitchExternalPower=suspend@HandleLidSwitchExternalPower=suspend@g' /etc/systemd/logind.conf
