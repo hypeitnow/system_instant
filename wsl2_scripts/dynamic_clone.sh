b3f6b0c9a86620aec4245324c74b838bc729d146
hypeit <matkoz111@gmail.com>
2024-10-12 15:46:18 +0200

    correction

--- wsl2_scripts/dynamic_clone.sh
+++ wsl2_scripts/dynamic_clone.sh
@@ -5,7 +5,8 @@ REPO_URL=$1
 if [[ "$REPO_URL" =~ github.com/circlek ]]; then
     echo "using gcm"
     git config --global credential.helper /usr/bin/git-credential-manager
-    git config --global credential.credentialStore 'cache --timeout=86400'
+    git config --global credential.credentialStore 'cache'
+    git config --global credential.cacheOptions "--timeout 86400"
 else
     git config --global credential.helper /usr/lib/git-core/git-credential-libsecret
     git config --global credential.credentialStore secretservice
@@ -15,3 +16,4 @@ git clone "$REPO_URL"
 
 # Reset the global credential.helper to a default value if needed
 git config --global credential.helper /usr/lib/git-core/git-credential-libsecret 
+git config --global credential.credentialStore secretservice
